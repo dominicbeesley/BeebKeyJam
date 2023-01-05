14ff737dcf0e4faaff1f5d1b822380b9dbd65213
dominicbeesley <dominic@authorityfile.co.uk>
2023-01-05 14:06:09 +0000

    RFB 1

--- src-rfb/beebkbd_rfb.c
+++ src-rfb/beebkbd_rfb.c
@@ -6,12 +6,15 @@
 #include "hardware/dma.h"
 #include "hardware/structs/bus_ctrl.h"
 #include "hardware/clocks.h"
+#include "rgb_cap.pio.h"
 
 
 const uint CAPTURE_PIN_SYNC = 17;
 const uint CAPTURE_PIN_BASE = 15;
 const uint CAPTURE_PIN_COUNT = 2;
-const uint CAPTURE_N_SAMPLES = 800;
+const uint CAPTURE_N_SAMPLES = 800*200;
+
+uint buf_size_words;
 
 static inline uint bits_packed_per_word(uint pin_count) {
     // If the number of pins to be sampled divides the shift register size, we
@@ -22,37 +25,33 @@ static inline uint bits_packed_per_word(uint pin_count) {
     return SHIFT_REG_WIDTH - (SHIFT_REG_WIDTH % pin_count);
 }
 
-void logic_analyser_init(PIO pio, uint sm, uint pin_base, uint pin_count, float div) {
-    // Load a program to capture n pins. This is just a single `in pins, n`
-    // instruction with a wrap.
-    uint16_t capture_prog_instr = pio_encode_in(pio_pins, pin_count);
-    struct pio_program capture_prog = {
-            .instructions = &capture_prog_instr,
-            .length = 1,
-            .origin = -1
-    };
-    uint offset = pio_add_program(pio, &capture_prog);
+void rgb_cap_init(PIO pio, uint sm, uint pin_base, uint pin_count) {
 
-    // Configure state machine to loop over this `in` instruction forever,
-    // with autopush enabled.
-    pio_sm_config c = pio_get_default_sm_config();
-    sm_config_set_in_pins(&c, pin_base);
-    sm_config_set_wrap(&c, offset, offset);
+    uint offset = pio_add_program(pio, &rgb_cap_program);
+
+    pio_sm_config c = rgb_cap_program_get_default_config(offset);
+    sm_config_set_in_pins(&c, pin_base); // for WAIT, IN
+    // Shift to right, autopush enabled
+    // SM transmits 1 pixel per 2 execution cycles.
+    float div = (float)clock_get_hz(clk_sys) / (2 * 16000000);
     sm_config_set_clkdiv(&c, div);
+    
     // Note that we may push at a < 32 bit threshold if pin_count does not
     // divide 32. We are using shift-to-right, so the sample data ends up
     // left-justified in the FIFO in this case, with some zeroes at the LSBs.
     sm_config_set_in_shift(&c, true, true, bits_packed_per_word(pin_count));
-    sm_config_set_fifo_join(&c, PIO_FIFO_JOIN_RX);
+    sm_config_set_fifo_join(&c, PIO_FIFO_JOIN_NONE);
     pio_sm_init(pio, sm, offset, &c);
+
 }
 
-void logic_analyser_arm(PIO pio, uint sm, uint dma_chan, uint32_t *capture_buf, size_t capture_size_words,
+void rgb_cap_arm(PIO pio, uint sm, uint dma_chan, uint32_t *capture_buf, size_t capture_size_words,
                         uint trigger_pin, bool trigger_level) {
     pio_sm_set_enabled(pio, sm, false);
     // Need to clear _input shift counter_, as well as FIFO, because there may be
     // partial ISR contents left over from a previous run. sm_restart does this.
     pio_sm_clear_fifos(pio, sm);
+    pio_sm_put_blocking(pio, sm, (800 << 16) | (200));
     pio_sm_restart(pio, sm);
 
     dma_channel_config c = dma_channel_get_default_config(dma_chan);
@@ -67,41 +66,65 @@ void logic_analyser_arm(PIO pio, uint sm, uint dma_chan, uint32_t *capture_buf, 
         true                // Start immediately
     );
 
-    pio_sm_exec(pio, sm, pio_encode_wait_gpio(trigger_level, trigger_pin));
+    //pio_sm_exec(pio, sm, pio_encode_wait_gpio(trigger_level, trigger_pin));
     pio_sm_set_enabled(pio, sm, true);
 }
 
-void print_capture_buf(const uint32_t *buf, uint pin_base, uint pin_count, uint32_t n_samples) {
-    // Display the capture buffer in text form, like this:
-    // 00: __--__--__--__--__--__--
-    // 01: ____----____----____----
-    printf("Capture:\n");
-    // Each FIFO record may be only partially filled with bits, depending on
-    // whether pin_count is a factor of 32.
-    uint record_size_bits = bits_packed_per_word(pin_count);
-    for (int pin = 0; pin < pin_count; ++pin) {
-        printf("%02d: ", pin + pin_base);
-        for (int sample = 0; sample < n_samples; ++sample) {
-            uint bit_index = pin + sample * pin_count;
-            uint word_index = bit_index / record_size_bits;
-            // Data is left-justified in each FIFO entry, hence the (32 - record_size_bits) offset
-            uint word_mask = 1u << (bit_index % record_size_bits + 32 - record_size_bits);
-            printf(buf[word_index] & word_mask ? "-" : "_");
-        }
-        printf("\n");
+void outout(const void *p, int l) {
+    const char * pp = p;
+    while (l-- > 0) {
+        putchar_raw(*(pp++));
     }
+}
+
+void print_capture_buf(const uint32_t *buf, uint pin_base, uint pin_count, uint32_t n_samples) {
+//    // Display the capture buffer in text form, like this:
+//    // 00: __--__--__--__--__--__--
+//    // 01: ____----____----____----
+//    printf("Capture:\n");
+//    // Each FIFO record may be only partially filled with bits, depending on
+//    // whether pin_count is a factor of 32.
+//    uint record_size_bits = bits_packed_per_word(pin_count);
+//    for (int pin = 0; pin < pin_count; ++pin) {
+//        printf("%02d: ", pin + pin_base);
+//        for (int sample = 0; sample < n_samples; ++sample) {
+//            uint bit_index = pin + sample * pin_count;
+//            uint word_index = bit_index / record_size_bits;
+//            // Data is left-justified in each FIFO entry, hence the (32 - record_size_bits) offset
+//            uint word_mask = 1u << (bit_index % record_size_bits + 32 - record_size_bits);
+//            printf(buf[word_index] & word_mask ? "-" : "_");
+//        }
+//        printf("\n");
+//    }
+
+        uint32_t marker = 0xFFFFFFFF;
+        outout(&marker, 4);
+        outout(buf, buf_size_words * sizeof(uint32_t));
 }
 
 int main() {
     stdio_init_all();
+
+    for (uint i = CAPTURE_PIN_BASE; i < CAPTURE_PIN_BASE + CAPTURE_PIN_COUNT; i++) {
+        gpio_init(i);
+        gpio_set_pulls(i,0,1);
+    }
+    gpio_init(CAPTURE_PIN_SYNC);
+    gpio_set_pulls(CAPTURE_PIN_SYNC,0,1);
+
+
+    getchar();
+
     printf("PIO logic analyser example\n");
+
+    getchar();
 
     // We're going to capture into a u32 buffer, for best DMA efficiency. Need
     // to be careful of rounding in case the number of pins being sampled
     // isn't a power of 2.
     uint total_sample_bits = CAPTURE_N_SAMPLES * CAPTURE_PIN_COUNT;
     total_sample_bits += bits_packed_per_word(CAPTURE_PIN_COUNT) - 1;
-    uint buf_size_words = total_sample_bits / bits_packed_per_word(CAPTURE_PIN_COUNT);
+    buf_size_words = total_sample_bits / bits_packed_per_word(CAPTURE_PIN_COUNT);
     uint32_t *capture_buf = malloc(buf_size_words * sizeof(uint32_t));
     hard_assert(capture_buf);
 
@@ -114,14 +137,14 @@ int main() {
     uint sm = 0;
     uint dma_chan = 0;
 
-    logic_analyser_init(pio, sm, CAPTURE_PIN_BASE, CAPTURE_PIN_COUNT, (float)clock_get_hz(clk_sys) / (16000000));
+    rgb_cap_init(pio, sm, CAPTURE_PIN_BASE, CAPTURE_PIN_COUNT);
 
     while(1) {
 
         getchar();
 
         printf("Arming trigger\n");
-        logic_analyser_arm(pio, sm, dma_chan, capture_buf, buf_size_words, CAPTURE_PIN_SYNC, false);
+        rgb_cap_arm(pio, sm, dma_chan, capture_buf, buf_size_words, CAPTURE_PIN_SYNC, false);
 
 
         // The logic analyser should have started capturing as soon as it saw the
@@ -131,5 +154,10 @@ int main() {
 
         print_capture_buf(capture_buf, CAPTURE_PIN_BASE, CAPTURE_PIN_COUNT, CAPTURE_N_SAMPLES);
 
+    }
+
+
+    while (1) {
+        printf("BOO\n");
     }
 }
---
+++ support/RemoteFB/RemoteFB/MainWindow.xaml.cs
@@ -0,0 +1,131 @@
+﻿using DossySerialPort;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RemoteFB
+{
+
+    /// <summary>
+    /// FxCop requires all Marshalled functions to be in a class called NativeMethods.
+    /// </summary>
+    internal static class NativeMethods
+    {
+        [DllImport("gdi32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        internal static extern bool DeleteObject(IntPtr hObject);
+    }
+
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            const int W = 1024;
+
+            try
+            {
+                using (var bm = new Bitmap(W, 200))
+                {
+
+
+                    using (var s = new DossySerial("COM8", 115200))
+                    {
+                        s.Write(new byte[] { 0xD, 65 }, 0, 2, 1000);
+
+                        byte[] buf = new byte[W / 4];
+
+
+
+                        for (int y = 0; y < 200; y++)
+                        {
+                            int n = s.Read(buf, W / 4, 1000);
+                            if (n != W / 4)
+                            {
+                                MessageBox.Show($"BOP {n}");
+                                return;
+                            }
+
+                            int x = 0;
+                            for (int j = 0; j < W / 4; j++)
+                            {
+                                byte b = buf[j];
+                                for (int i = 0; i < 4; i++)
+                                {
+                                    bm.SetPixel(x++, y, ((b & 0x01) != 0) ? System.Drawing.Color.White : System.Drawing.Color.Black);
+                                    b = (byte)(b >> 2);
+                                }
+
+                            }
+
+                        }
+
+                        piccer.Source = ToBitmapSource(bm);
+
+                    }
+                }
+            }
+            catch (Exception ex) {
+
+                MessageBox.Show(ex.ToString());
+            }
+
+        }
+
+
+        /// <summary>
+        /// Converts a <see cref="System.Drawing.Bitmap"/> into a WPF <see cref="BitmapSource"/>.
+        /// </summary>
+        /// <remarks>Uses GDI to do the conversion. Hence the call to the marshalled DeleteObject.
+        /// </remarks>
+        /// <param name="source">The source bitmap.</param>
+        /// <returns>A BitmapSource</returns>
+        public BitmapSource ToBitmapSource(System.Drawing.Bitmap source)
+        {
+            BitmapSource bitSrc = null;
+
+            var hBitmap = source.GetHbitmap();
+
+            try
+            {
+                bitSrc = System.Windows.Interop.Imaging.CreateBitmapSourceFromHBitmap(
+                    hBitmap,
+                    IntPtr.Zero,
+                    Int32Rect.Empty,
+                    BitmapSizeOptions.FromEmptyOptions());
+            }
+            catch (Win32Exception)
+            {
+                bitSrc = null;
+            }
+            finally
+            {
+                NativeMethods.DeleteObject(hBitmap);
+            }
+
+            return bitSrc;
+        }
+    }
+}
